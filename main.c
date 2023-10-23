b2f71143d2bbb57d6fec0ee88e9a7ef61356c675
For8stt <xkisil@stuba.sk>
2023-10-23 10:48:51 +0200

    pokúsi otvoriť subor

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
+int v(){
+    FILE *f;
+    if((f= fopen("dataloger.txt","r"))!=NULL){
+        printf("súbor už bol otvorený\n");
+        fclose(f);
+    }else{
+        if((f= fopen("dataloger.txt","w"))!=NULL) {
+            printf("Subor bol uspesne otvoreny");
+            fclose(f);
+        }else{
+            printf("Neotvoreny subor\n");
+        }
+    }
     return 0;
+
+};
+int n(){
+
+    return 0;
+}
+int main() {
+    char option;
+    scanf("%c",&option);
+    switch (option) {
+        case 'v':
+            v();
+        case 'n':
+            n();
+            break;
+        default:
+            printf("Pomylka");
+    }
+    return 0;
+
 }
