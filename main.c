fdfc2f204035f87b4390b3d413efd881eb895f96
For8stt <xkisil@stuba.sk>
2023-10-25 12:49:00 +0200

    urbil som dynamicky polia pre jednotlivé položky zo vstupu

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int v(){
-    FILE *f;
+void uvolni(char** polia, int row);
+
+int v(FILE*f){
+    //program zistí, či txt súbor už bol otvorený
     if((f= fopen("dataloger.txt","r"))!=NULL){
         printf("súbor už bol otvorený\n");
         fclose(f);
@@ -13,25 +17,57 @@ int v(){
             printf("Neotvoreny subor\n");
         }
     }
-    return 0;
-
-};
-int n(){
 
     return 0;
 }
+int n(FILE*f){
+    if (f == NULL) {//Ak súbor nie je otvorený(t.j. ešte nebol vykonaný príkaz ‘v’)
+        fprintf(stderr,"Neotvoreny subor.\n");
+        return 1;
+    }
+
+    char line[100];
+    int row=0;
+    while (fgets(line,sizeof(line),f)!=NULL){
+        ++row;
+    }
+    rewind(f);
+
+    char **polia=NULL;
+    if (polia != NULL) { // Uvoľnite pamäť, ak pole už existuje
+        //uvolni(polia, row);
+        for (int i = 0; i < row; i++) {
+            free(polia[i]);
+        }
+        free(polia);
+    }
+
+    polia=(char**)malloc(row*sizeof(line));
+    for (int i = 0; i < row; i++) {//Zapís takom poradí, v akom sú v textovom súbore
+        fgets(line, sizeof(line), f);
+        polia[i] = strdup(line);
+    }
+    uvolni(polia,row);
+    return 0;
+}
+
+
+void uvolni(char**polia,int row){
+    for (int i = 0; i < row; ++i) {
+        free(polia[i]);
+    }
+    free(polia);
+}
 int main() {
+    FILE *f=NULL;
     char option;
-    scanf("%c",&option);
-    switch (option) {
-        case 'v':
-            v();
-        case 'n':
-            n();
-            break;
-        default:
-            printf("Pomylka");
+    scanf("%c", &option);
+
+    if(option=='v'){
+        v(f);
+    }
+    if(option=='n'){
+        n(f);
     }
     return 0;
-
 }
